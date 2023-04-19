256068842cb1362923ee84111d73fc1d57e5dda6
e.yari@inotek.ir <e.yari@inotek.ir>
2023-04-19 11:54:52 +0330

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      "light-gray": "hsl(212, 45%, 89%)",
+      "grayish-blue": "hsl(220, 15%, 55%)",
+      "dark-blue": "hsl(218, 44%, 22%)",
+    },
+    fontFamily: {
+      outfit: ["Outfit", "sans-serif"],
+    },
+    fontWeight: {
+      normal: "400",
+      bold: "700",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
